fcf71810b2504050753b1ee556e12bac5bb7c8f2
Sukriti Chawla <sukriti.gmail.com>
2025-02-10 15:38:02 +0530

    app.js

--- lsr/Frontend/src/App.js
+++ lsr/Frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import HomePage from "./Pages/HomePage.js";
 
 // Homepage Component
 function Home() {
@@ -18,36 +19,20 @@ function Home() {
   );
 }
 
-// About Page Component
-function About() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>About Us</h1>
-        <p>We are here to make your experience better.</p>
-        <Link to="/">
-          <button className="App-link">Go Home</button>
-        </Link>
-      </header>
-    </div>
-  );
-}
+
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Link to="/" className="App-link">Home</Link>
-          <Link to="/about" className="App-link">About</Link>
-        </nav>
+      
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="seatMap" element={<Home />} />
+
+          
         </Routes>
-      </div>
+      
     </Router>
   );
 }
