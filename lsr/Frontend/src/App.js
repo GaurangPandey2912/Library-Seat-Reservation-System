100f8617c0781c3e41a9350a209d8116fbad095f
Sukriti Chawla <sukriti.gmail.com>
2025-02-10 15:51:05 +0530

    route to seatmap

--- lsr/Frontend/src/App.js
+++ lsr/Frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./Pages/HomePage.js";
+import SeatReservation from "./Pages/Seatmap.js";
 
 // Homepage Component
 function Home() {
@@ -28,7 +29,8 @@ function App() {
 
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="seatMap" element={<Home />} />
+          <Route path="/seatMap" element={<SeatReservation />} />
+
 
           
         </Routes>
