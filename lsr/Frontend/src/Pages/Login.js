07118b69fba45a465fbb98f89867368dbb9ec0f2
Sukriti Chawla <sukriti.gmail.com>
2025-02-11 08:34:14 +0530

    home button on sign in

--- lsr/Frontend/src/Pages/Login.js
+++ lsr/Frontend/src/Pages/Login.js
@@ -48,7 +48,7 @@ const Login = () => {
           <img src="/brandlogo.png" alt="Brand Logo" className="brand-logo" />
         </div>
         <ul>
-          <li><a href="#home">Home</a></li>
+          <li><a href="/">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#contact">Contact</a></li>
           <li>
